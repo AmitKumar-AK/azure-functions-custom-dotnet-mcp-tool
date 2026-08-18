f766725a1ffec387897b723b1e16afb358095e03
AmitKumar-AK <amitkumar_mca04@hotmail.com>
2026-08-19 00:12:07 +0100

    Initial changes

---
+++ EnterpriseMcpServer/Program.cs
@@ -0,0 +1,18 @@
+using EnterpriseMcpServer.Services;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = FunctionsApplication.CreateBuilder(args);
+
+builder.ConfigureFunctionsWebApplication();
+
+builder.Services
+    .AddApplicationInsightsTelemetryWorkerService()
+    .ConfigureFunctionsApplicationInsights();
+
+// Register application services
+builder.Services.AddScoped<IDataverseService, DataverseService>();
+
+builder.Build().Run();
---
+++ EnterpriseMcpServer/Services/IDataverseService.cs
@@ -0,0 +1,20 @@
+﻿
+using EnterpriseMcpServer.Models;
+
+namespace EnterpriseMcpServer.Services;
+
+/// <summary>
+/// Service interface for managing enquiry records in Microsoft Dataverse.
+/// Provides CRUD operations and query capabilities for enquiry data.
+/// </summary>
+public interface IDataverseService
+{
+
+    /// <summary>
+    /// Retrieves multiple enquiries from Dataverse, ordered by creation date descending.
+    /// </summary>
+    /// <param name="topCount">The maximum number of enquiries to retrieve. Default is 100.</param>
+    /// <param name="cancellationToken">Token to cancel the operation.</param>
+    /// <returns>A collection of enquiry responses.</returns>
+    Task<IEnumerable<EnquiryResponse>> GetEnquiriesAsync(int topCount = 100, CancellationToken cancellationToken = default);
+}
